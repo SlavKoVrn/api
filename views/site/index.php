a37f895e9ae8d32cf6b8dae24fcd4c753be97224
SlavKoVrn <slavko.chita@gmail.com>
2020-07-12 06:02:17 +0300

    frontend gridview

--- views/site/index.php
+++ views/site/index.php
@@ -1,51 +1,48 @@
 <?php
+use app\modules\api\models\Messages;
+use app\modules\api\models\Authors;
+
+use yii\data\ActiveDataProvider;
+use yii\grid\GridView;
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Yii2 API RESTfull';
+
+$query = new \yii\db\Query();
+$query->select(['a.phone','m.content','m.datetime'])
+    ->from(['m'=>Messages::tableName()])
+    ->innerJoin(['a'=>Authors::tableName()],'m.author_id=a.id');
+$dataProvider = new ActiveDataProvider([
+    'query' => $query,
+    'pagination' => ['pageSize' => 20],
+]);
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        <h1>Yii2 API RESTfull</h1>
     </div>
 
     <div class="body-content">
 
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <div class="col-lg-12">
+                <?php yii\widgets\Pjax::begin(['id' => 'messages']) ?>
+                <?= GridView::widget([
+                    'dataProvider' => $dataProvider,
+                    'columns' => [
+                        'phone',
+                        'content',
+                        [
+                            'attribute'=>'datetime',
+                            'value'=>function($model){
+                                return date('d.m.Y H:i',strtotime($model['datetime']));
+                            }
+                        ]
+                    ]
+                ]) ?>
+                <?php yii\widgets\Pjax::end() ?>
             </div>
         </div>
 
